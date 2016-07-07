da204aa5dbbf952e70886c99123f29a444968464
Chew <chewsterchew@gmail.com>
2016-07-07 19:52:06 -0400

    Add a beta memedb.
    Memes.

--- chewbot.rb
+++ chewbot.rb
@@ -1,6 +1,7 @@
 require 'discordrb'
 
 bot = Discordrb::Bot.new token: '[your token]]', application_id: [your application id]
+
 bot.message(with_text: '%^ping') do |event|
   event.respond 'Pong!'
 end
@@ -27,16 +28,28 @@ bot.message(starting_with: '%^help') do |event|
   %^code - Shows Code
   %^rate - Rate something /10
   %^ping - Is the bot online?
-  %^who is champ - Guess, take a guess.```'
+  %^who is champ - Guess, take a guess.
+  %^namemc - Search the NameMC database.
+  %^memedb - Find a meme in the meme database.
+  %^memedb submit - Submit a meme to the meme database.```'
 end
 bot.message(starting_with: '(╯°□°）╯︵ ┻━┻') do |event|
   event.respond 'Get anger management.'
 end
-bot.message(starting_with: '%^rickroll') do |event|
-  event.respond 'https://media.giphy.com/media/AXQaLoWMeSmRy/giphy.gif'
-end
 bot.message(starting_with: '%^namemc') do |event|
   _, *namemc = event.message.content.split
   event.respond "NameMC Search: http://namemc.com/s/#{namemc.join(' ')}"
+end
+bot.message(starting_with: '%^memedb rickroll') do |event|
+  event.respond 'https://media.giphy.com/media/AXQaLoWMeSmRy/giphy.gif'
+end
+bot.message(starting_with: '%^memedb deanmeme') do |event|
+  event.respond 'http://zounce.ga/deanmemes.png'
+end
+bot.message(with_text: '%^memedb') do |event|
+  event.respond 'Check out the meme database here: [Link]'
+end
+bot.message(starting_with: '%^memedb submit') do |event|
+  event.respond 'Submit your meme for the database here: http://goo.gl/forms/BRMomYVizsY7SqOg2'
 end
 bot.run
