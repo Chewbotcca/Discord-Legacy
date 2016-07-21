b48185cc4d8aab943b1e66cdef12c148ad5ad784
Chew <chewsterchew@gmail.com>
2016-07-21 10:47:28 -0400

    added stats and rip

--- chewbot.rb
+++ chewbot.rb
@@ -2,33 +2,32 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: '[your token]', application_id: [your application id]
 
+bot.message(starting_with: '%^help') do |event|
+  event.respond '```Command List:
+  %^ping - Is the bot online? Well, yes, you issued this command..
+  %^help - Shows This.
+  %^invite - Invite the bot to your server.
+  %^code - Shows Code.
+  %^rate - Rate something /10.
+  %^namemc - Search the NameMC database.
+  %^memedb - Find a meme in the meme database.
+  %^memedb submit - Submit a meme to the meme database.
+  %^stats - Show some stats for Chewbotcca
+  %^rip - RIP a user! Do not mention them or link will also rip```'
+end
 bot.message(with_text: '%^ping') do |event|
   event.respond 'Pong!'
-end
-bot.message(with_text: '%^who is champ') do |event|
-  event.respond 'John Cena!'
 end
 bot.message(starting_with: '%^rate') do |event|
   _, *rating = event.message.content.split
   event.respond "#{event.user.mention} has rated #{rating.join(' ')}/10."
 end
 bot.message(with_text: '%^code') do |event|
-  event.respond 'Chewbotcca was written in discordrb, and was made by Chew. Source code here: http://github.com/Chewsterchew/Chewbotcca'
+  event.respond 'Chewbotcca was written in discordrb, and was made by @Chew#6216. Source code here: http://github.com/Chewsterchew/Chewbotcca'
 end
 bot.message(starting_with: '%^invite') do |event|
   event.respond 'Hello! Invite me to your server here: http://chcra.site/Chewbotcca
   Join our support here right here: https://discord.gg/Q8TazNz'
-end
-bot.message(starting_with: '%^help') do |event|
-  event.respond '```Command List:
-  %^help - Shows This
-  %^code - Shows Code
-  %^rate - Rate something /10
-  %^ping - Is the bot online?
-  %^who is champ - Guess, take a guess.
-  %^namemc - Search the NameMC database.
-  %^memedb - Find a meme in the meme database.
-  %^memedb submit - Submit a meme to the meme database.```'
 end
 bot.message(starting_with: '%^namemc') do |event|
   _, *namemc = event.message.content.split
@@ -71,5 +70,12 @@ bot.message(starting_with: '%^memedb pokesteak') do |event|
 end
 bot.message(starting_with: '%^memedb losthope') do |event|
   event.respond "http://memedb.chewcraft.me/memes/losthope.png submitted by Asymmetrically"
+end
+bot.message(starting_with: '%^stats') do |event|
+  event.respond "I'm currently on #{event.bot.servers.count} servers! Add me to yours with `%^invite`"
+end
+bot.message(starting_with: '%^rip') do |event|
+  _, *rip = event.message.content.split
+  event.respond "#{rip.join(' ')} got #rekt! http://ripme.xyz/#{rip.join(' ')}"
 end
 bot.run
