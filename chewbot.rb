16a43c87a1b7e30c7390ba97428598527d2f85b6
Chewsterchew <chew@chew.pw>
2017-08-26 14:13:03 -0500

    Using xml, added a new randomcat url

--- chewbot.rb
+++ chewbot.rb
@@ -2,6 +2,8 @@ require 'discordrb'
 require 'rest-client'
 require 'json'
 require 'yaml'
+require 'nokogiri'
+require 'open-uri'
 puts "All dependicies loaded"
 
 CONFIG = YAML.load_file('config.yaml')
@@ -280,7 +282,9 @@ bot.ready do |event|
 end
 
 bot.command(:cat) do |event|
-  event.respond "#{["Aww!","Adorable."].sample} #{JSON.parse(RestClient.get('http://random.cat/meow'))['file']}"
+  jsoncat = JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+  xmlcat = Nokogiri::XML(open("http://thecatapi.com/api/images/get?format=xml&results_per_page=1")).xpath('//url').text
+  event.respond "#{["Aww!","Adorable."].sample} #{[jsoncat,xmlcat].sample}"
 end
 
 bot.command(:catfact) do |event|
