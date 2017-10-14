7529f96dfd5f3475db028f77dbc1115fc05ff0f1
Advaith <advaith1@users.noreply.github.com>
2017-10-14 05:47:49 -0700

    seconds to milliseconds

--- lib/chewbot.rb
+++ lib/chewbot.rb
@@ -67,10 +67,10 @@ end
 # Ping
 bot.command(:ping) do |event, noedit|
   if noedit == 'noedit'
-    event.respond "Pong! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} seconds."
+    event.respond "Pong! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} milliseconds."
   else
     m = event.respond('Pinging...')
-    m.edit "Pong! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} seconds."
+    m.edit "Pong! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} milliseconds."
   end
 end
 
