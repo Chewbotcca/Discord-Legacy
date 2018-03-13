4c8deb537c5dddf405491801632ffc6d6da196ed
Chewsterchew <chew@chew.pw>
2018-03-13 15:05:47 -0500

    Add embeds to stats

--- lib/plugins/stats.rb
+++ lib/plugins/stats.rb
@@ -6,13 +6,18 @@ module Stats
     mm, ss = t.divmod(60)
     hh, mm = mm.divmod(60)
     dd, hh = hh.divmod(24)
-    event << 'Chewbotcca - A basic, yet functioning, discord bot.'
-    event << 'Author: Chew#6216 [116013677060161545]'
-    event << 'Code: <http://github.com/Chewbotcca/Discord>'
-    event << 'Bot Version: 1.0.0'
-    event << 'Library: discordrb 3.2.1'
-    event << "Server Count: #{event.bot.servers.count}"
-    event << "Total User Count: #{event.bot.users.count}"
-    event << format('Uptime: %d days, %d hours, %d minutes and %d seconds', dd, hh, mm, ss)
+
+    event.channel.send_embed do |e|
+      e.title = "Chewbotcca - A basic, yet functioning, discord bot"
+
+      e.add_field(name: 'Author', value: "Chew#6216\n[116013677060161545]", inline: true)
+      e.add_field(name: 'Code:', value: '<http://github.com/Chewbotcca/Discord>', inline: true)
+      e.add_field(name: 'Bot Version', value: '1.0.0', inline: true)
+      e.add_field(name: 'Library', value: 'discordrb 3.2.1', inline: true)
+      e.add_field(name: 'Server Count:', value: event.bot.servers.count, inline: true)
+      e.add_field(name: 'Total User Count:', value: event.bot.users.count, inline: true)
+      e.add_field(name: 'Uptime:', value: format('%d days, %d hours, %d minutes and %d seconds', dd, hh, mm, ss), inline: true)
+      e.color = '00FF00'
+    end
   end
 end
