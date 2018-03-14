ebfc71b027fb90be6b5d797a865c644754e1c04a
Chewsterchew <chew@chew.pw>
2018-03-14 14:24:22 -0500

    Don't embed command link

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -2,6 +2,6 @@ module Help
   extend Discordrb::Commands::CommandContainer
 
   command(%i[help commands]) do |event|
-    event.respond 'You can find all my commands here: https://discord.chewbotcca.co/commands'
+    event.respond 'You can find all my commands here: <https://discord.chewbotcca.co/commands>'
   end
 end
