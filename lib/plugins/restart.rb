ad6d85f958df6692ef0c1f5b155e5a6914a21eb1
Chewsterchew <chew@chew.pw>
2018-03-14 12:19:27 -0500

    Brought over code from Chewbotcca/IRC for the restart command

--- lib/plugins/restart.rb
+++ lib/plugins/restart.rb
@@ -1,45 +1,49 @@
 module Restart
   extend Discordrb::Commands::CommandContainer
 
-  command(:restart, min_args: 1, max_args: 1) do |event, task|
-    task.downcase!
-    if task == 'help'
-      event << 'Possible arguments'
-      event << '`pushlocal` - Pushes local repository to github. Usually not used.'
-      event << '`restartonly` - Restarts bot without pulling code or anything'
-      event << '`update` - Restarts the bot and updates the bot'
-      event << '`pushonly` - Pushes code (must `git add` and `git commit`)'
+  command(:restart) do |event|
+    unless event.user.id == CONFIG['owner_id']
+      event.respond "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
+      break
+    end
+    event.respond 'Restarting the bot without updating...'
+    sleep 1
+    exec('ruby run.rb')
+  end
+
+  command(:update) do |event|
+    unless event.user.id == CONFIG['owner_id']
+      event.respond "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
+      return
+    end
+    event.respond 'Restarting and Updating!'
+    sleep 1
+    `git pull`
+    exec('ruby run.rb')
+  end
+
+  command(:updates) do |event|
+    `git fetch` if event.user.id == CONFIG['owner_id']
+    response = `git rev-list origin/master | wc -l`.to_i
+    commits = `git rev-list master | wc -l`.to_i
+    if commits.zero?
+      event.respond "Your machine doesn't support git or it isn't working!"
+      break
     end
     if event.user.id == CONFIG['owner_id']
-      event.respond 'Restarting not supported on Windows!' if CONFIG['os'] == 'Windows'
-      if CONFIG['os'] == ('Mac' || 'Linux')
-        if task == 'update'
-          begin
-            event.respond 'Restarting and Updating!'
-            exec('bash scripts/update.sh')
-          end
-        end
-        if task == 'pushlocal'
-          begin
-            event.respond 'Restarting and uploading all that fancy local code'
-            exec('bash scripts/push.sh')
-          end
-        end
-        if task == 'restartonly'
-          begin
-            event.respond 'Restarting the bot without updating...'
-            exec('bash scripts/restartonly.sh')
-          end
-        end
-        if task == 'pushonly'
-          begin
-            event.respond 'Restarting the bot using saved commits...'
-            exec('bash scripts/pushonly.sh')
-          end
+      event.channel.send_embed do |e|
+        e.title = "You are running Chewbotcca commit #{commits}"
+        if response == commits
+          e.description = 'You are running the latest commit.'
+          e.color = '00FF00'
+        elsif response < commits
+          e.description = "You are running an un-pushed commit! Are you a developer? (Most Recent: #{response})"
+          e.color = 'FFFF00'
+        else
+          e.description = "You are #{response - commits} commit(s) behind! Run `%^update` to update"
+          e.color = 'FF0000'
         end
       end
-    else
-      event.respond "You can't restart! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to restart the bot!)"
     end
   end
 end
