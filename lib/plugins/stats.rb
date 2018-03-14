f5ed97634d30a2d598d98cf1b48ebfced0c7f563
Chewsterchew <chew@chew.pw>
2018-03-14 12:26:36 -0500

    Make uptime look better

--- lib/plugins/stats.rb
+++ lib/plugins/stats.rb
@@ -6,6 +6,10 @@ module Stats
     mm, ss = t.divmod(60)
     hh, mm = mm.divmod(60)
     dd, hh = hh.divmod(24)
+    days = format("%d days\n", dd) if dd != 0
+    hours = format("%d hours\n", hh) if hh != 0
+    mins = format("%d minutes\n", mm) if mm != 0
+    secs = format('%d seconds', ss) if ss != 0
 
     commits = `git rev-list master | wc -l`.to_i
     version = `git describe --abbrev=0 --tags`.to_s
@@ -25,9 +29,9 @@ module Stats
       e.add_field(name: 'Code', value: '<http://github.com/Chewbotcca/Discord>', inline: true)
       e.add_field(name: 'Bot Version', value: botversion, inline: true) unless botversion == ''
       e.add_field(name: 'Library', value: 'discordrb 3.2.1', inline: true)
+      e.add_field(name: 'Uptime', value: "#{days}#{hours}#{mins}#{secs}", inline: true)
       e.add_field(name: 'Server Count', value: event.bot.servers.count, inline: true)
       e.add_field(name: 'Total User Count', value: event.bot.users.count, inline: true)
-      e.add_field(name: 'Uptime', value: format('%d days, %d hours, %d minutes and %d seconds', dd, hh, mm, ss), inline: true)
       e.color = '00FF00'
     end
   end
