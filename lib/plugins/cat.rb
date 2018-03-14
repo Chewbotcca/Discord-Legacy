65197028faee9b12feed0a31d364301b12aee443
Chewsterchew <chew@chew.pw>
2018-03-14 13:36:31 -0500

    Update cat api url

--- lib/plugins/cat.rb
+++ lib/plugins/cat.rb
@@ -4,7 +4,7 @@ module Cat
   command(:cat) do |event|
     whichcat = rand(2)
     showcat = if whichcat.zero?
-                JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+                JSON.parse(RestClient.get('http://aws.random.cat/meow'))['file']
               else
                 Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
               end
