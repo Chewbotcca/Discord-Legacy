d8e5664eaaeefa4372db16d8a761470b82682d45
Chewsterchew <chew@chew.pw>
2018-03-15 18:37:08 -0500

    Show recent commits when running %^update

--- lib/plugins/restart.rb
+++ lib/plugins/restart.rb
@@ -37,10 +37,10 @@ module Restart
           e.description = 'You are running the latest commit.'
           e.color = '00FF00'
         elsif response < commits
-          e.description = "You are running an un-pushed commit! Are you a developer? (Most Recent: #{response})"
+          e.description = "You are running an un-pushed commit! Are you a developer? (Most Recent: #{response})\n**Here are the 5 most recent commits.**\n#{`git log origin/master..master --pretty=format:\"[%h](http://github.com/Chewbotcca/Discord/commit/%H) - %s\" -5`}"
           e.color = 'FFFF00'
         else
-          e.description = "You are #{response - commits} commit(s) behind! Run `%^update` to update"
+          e.description = "You are #{response - commits} commit(s) behind! Run `%^update` to update.\n**Here are the 5 most recent commits.**\n#{`git log master..origin/master --pretty=format:\"[%h](http://github.com/Chewbotcca/Discord/commit/%H) - %s\" -5`}"
           e.color = 'FF0000'
         end
       end
