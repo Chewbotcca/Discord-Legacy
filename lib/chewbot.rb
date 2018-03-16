61d2e10e47e711fd4026a963e3234dc9a01fb55f
Chewsterchew <chew@chew.pw>
2018-03-16 17:21:29 -0500

    use constant for bot definition

--- lib/chewbot.rb
+++ lib/chewbot.rb
@@ -9,7 +9,7 @@ puts 'All dependicies loaded'
 CONFIG = YAML.load_file('config.yaml')
 puts 'Config loaded from file'
 
-bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'], client_id: CONFIG['client_id'], prefix: ["<@#{CONFIG['client_id']}> ", '%^']
+Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'], client_id: CONFIG['client_id'], prefix: ["<@#{CONFIG['client_id']}> ", '%^']
 
 puts 'Initial Startup complete, loading all plugins...'
 
@@ -22,37 +22,37 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
   command = bob[0][7..bob[0].length]
   command.delete!("\n")
   command = Object.const_get(command)
-  bot.include! command
+  Bot.include! command
   puts "Plugin #{command} successfully loaded!"
 end
 
 puts 'Done loading plugins! Finalizing start-up'
 
-bot.server_create do |event|
+Bot.server_create do |event|
   event.bot.game = "on #{event.bot.servers.count} servers | %^help"
   RestClient.post("https://discordbots.org/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotsorg'], :'Content-Type' => :json) unless CONFIG['dbotsorg'].nil?
   RestClient.post("https://bots.discord.pw/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotspw'], :'Content-Type' => :json)
 end
 
-bot.server_delete do |event|
+Bot.server_delete do |event|
   event.bot.game = "on #{event.bot.servers.count} servers | %^help"
   RestClient.post("https://discordbots.org/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotsorg'], :'Content-Type' => :json) unless CONFIG['dbotsorg'].nil?
   RestClient.post("https://bots.discord.pw/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotspw'], :'Content-Type' => :json) unless CONFIG['dbotspw'].nil?
 end
 
-bot.command(:connect) do |event|
+Bot.command(:connect) do |event|
   channel = event.user.voice_channel
   next "You're not in any voice channel!" unless channel
-  bot.voice_connect(channel)
+  Bot.voice_connect(channel)
   "Connected to voice channel: #{channel.name}."
 end
 
-bot.ready do |event|
+Bot.ready do |event|
   sleep 10
-  bot.game = "on #{event.bot.servers.count} servers | %^help"
+  Bot.game = "on #{event.bot.servers.count} servers | %^help"
   RestClient.post("https://discordbots.org/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotsorg'], :'Content-Type' => :json) unless CONFIG['dbotsorg'].nil?
   RestClient.post("https://bots.discord.pw/api/bots/#{CONFIG['client_id']}/stats", '{"server_count":' + event.bot.servers.count.to_s + '}', :Authorization => CONFIG['dbotspw'], :'Content-Type' => :json) unless CONFIG['dbotspw'].nil?
 end
 
 puts 'Bot is ready!'
-bot.run
+Bot.run
