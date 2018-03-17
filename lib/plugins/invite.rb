a42b3fa35a2f077f778cd3ef069ef6a08cf11902
Chewsterchew <chew@chew.pw>
2018-03-17 10:33:37 -0500

    Bring back the help server!! Geez

--- lib/plugins/invite.rb
+++ lib/plugins/invite.rb
@@ -2,6 +2,6 @@ module Invite
   extend Discordrb::Commands::CommandContainer
 
   command :invite do |event|
-    event.respond 'Hello! Invite me to your server here: <http://bit.ly/Chewbotcca>.'
+    event.respond 'Hello! Invite me to your server here: <http://bit.ly/Chewbotcca>. Join the help server here: https://discord.gg/Q8TazNz'
   end
 end
