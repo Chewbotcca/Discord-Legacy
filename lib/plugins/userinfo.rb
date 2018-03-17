19d7549ab11ee1805885ed66f7d086ee9d01d071
Chewsterchew <chew@chew.pw>
2018-03-17 10:19:31 -0500

    Userinfo now works if you mention someone gives you their info NICE

--- lib/plugins/userinfo.rb
+++ lib/plugins/userinfo.rb
@@ -1,16 +1,34 @@
 module UserInfo
   extend Discordrb::Commands::CommandContainer
 
-  command(%i[uinfo userinfo]) do |event|
+  command(%i[uinfo userinfo], max_args: 1) do |event, mention|
+    if !mention.nil?
+      userid = Bot.parse_mention(mention.to_s).id.to_i
+      user = Bot.user(userid)
+    else
+      user = event.user
+    end
     event.channel.send_embed do |e|
-      e.title = 'User Info for you!'
-      e.thumbnail = { url: "https://cdn.discordapp.com/avatars/#{event.user.id}/#{event.user.avatar_id}.webp?size=1024".to_s }
+      e.title = if mention.nil?
+                  'User Info for you!'
+                else
+                  "User info for #{user.name}"
+                end
+      e.thumbnail = { url: "https://cdn.discordapp.com/avatars/#{user.id}/#{user.avatar_id}.webp?size=1024".to_s }
 
-      e.add_field(name: 'Name#Discrim', value: "#{event.user.name}\##{event.user.discrim}", inline: true)
-      e.add_field(name: 'User ID:', value: event.user.id, inline: true)
-      e.add_field(name: 'Status', value: event.user.status, inline: true)
-      e.add_field(name: 'Nickname', value: event.user.nick, inline: true) unless event.user.nick.nil?
-      e.add_field(name: 'Currently Playing:', value: event.user.game, inline: true) unless event.user.game.nil?
+      e.add_field(name: 'Name#Discrim', value: "#{user.name}\##{user.discrim}", inline: true)
+      e.add_field(name: 'User ID', value: user.id, inline: true)
+      e.add_field(name: 'Status', value: user.status, inline: true)
+      begin
+        e.add_field(name: 'Nickname', value: user.nick, inline: true) unless user.nick.nil?
+      rescue StandardError
+        puts 'whoopsiedaisy this guy dont got no nickname'
+      end
+      begin
+        e.add_field(name: 'Currently Playing', value: user.game, inline: true) unless user.game.nil?
+      rescue StandardError
+        puts 'whoopsiedaisy this guy dont got no current playing'
+      end
       e.color = '00FF00'
     end
   end
