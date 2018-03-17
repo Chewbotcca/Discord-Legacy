9d442012bef5d69f765600b5285eeeb1038ce1a9
Chewsterchew <chew@chew.pw>
2018-03-17 10:34:28 -0500

    Merge MCAvatar plugin into the Minecraft Plugin

--- lib/plugins/mcavatar.rb
+++
@@ -1,7 +0,0 @@
-module MCavatar
-  extend Discordrb::Commands::CommandContainer
-
-  command(:mcavatar, min_args: 1, max_args: 1) do |event, mcuser|
-    event.respond "Alright, here is a 3D full view of the player for the skin: #{mcuser}. https://visage.surgeplay.com/full/512/#{mcuser}.png"
-  end
-end
--- lib/plugins/minecraft.rb
+++ lib/plugins/minecraft.rb
@@ -31,4 +31,8 @@ module Minecraft
       e.color = '00FF00'
     end
   end
+
+  command(:mcavatar, min_args: 1, max_args: 1) do |event, mcuser|
+    event.respond "Alright, here is a 3D full view of the player for the skin: #{mcuser}. https://visage.surgeplay.com/full/512/#{mcuser}.png"
+  end
 end
