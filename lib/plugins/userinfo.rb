2a29051f859ff06c11d85449ad442619a4dec6ef
Chewsterchew <chew@chew.pw>
2018-03-21 15:59:28 -0500

    Just use their avatar url...

--- lib/plugins/userinfo.rb
+++ lib/plugins/userinfo.rb
@@ -15,7 +15,7 @@ module UserInfo
                   else
                     "User info for #{user.name}"
                   end
-        e.thumbnail = { url: "https://cdn.discordapp.com/avatars/#{user.id}/#{user.avatar_id}.webp?size=1024".to_s }
+        e.thumbnail = { url: user.avatar_url.to_s }
 
         e.add_field(name: 'Name#Discrim', value: "#{user.name}\##{user.discrim}", inline: true)
         e.add_field(name: 'User ID', value: user.id, inline: true)
