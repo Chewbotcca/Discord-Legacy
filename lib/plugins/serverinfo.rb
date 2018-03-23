75d544467b4c689b1883826bf2eea689b79ae9de
Chewsterchew <chew@chew.pw>
2018-03-23 18:14:01 -0500

    Turns out icon_url returns jpg, yuck.

--- lib/plugins/serverinfo.rb
+++ lib/plugins/serverinfo.rb
@@ -6,17 +6,18 @@ module ServerInfo
       event.channel.send_embed do |e|
         e.title = 'Server Information'
 
-        e.thumbnail = { url: event.server.icon_url.to_s }
+        e.thumbnail = { url: "https://cdn.discordapp.com/icons/#{message.guild.id}/#{message.guild.icon}.png?size=1024".to_s }
 
         e.add_field(name: 'Server Name:', value: event.server.name, inline: true)
-        e.add_field(name: 'Server ID:', value: event.server.id, inline: true)
-        e.add_field(name: 'Server Region:', value: event.server.region, inline: true)
-        e.add_field(name: 'Server Owner', value: "#{event.server.owner.name}\##{event.server.owner.discrim}", inline: true)
-        e.add_field(name: 'Member Count:', value: event.server.members.count, inline: true)
+        e.add_field(name: 'Server ID', value: event.server.id, inline: true)
+        e.add_field(name: 'Server Region', value: event.server.region, inline: true)
+        e.add_field(name: 'Server Owner', value: event.server.owner.mention, inline: true)
+        e.add_field(name: 'Member Count', value: event.server.members.count, inline: true)
         e.add_field(name: 'Total Channel Count', value: event.server.channels.count, inline: true)
         e.add_field(name: 'Text Channels', value: event.server.text_channels.count, inline: true)
         e.add_field(name: 'Voice Channels', value: event.server.voice_channels.count, inline: true)
         e.add_field(name: 'Roles', value: event.server.roles.count, inline: true)
+
         e.color = '00FF00'
       end
     rescue Discordrb::Errors::NoPermission
