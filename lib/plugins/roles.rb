e9c169172602f7bc3cb52e0d30f6e2b6e5a3fad3
Chewsterchew <chew@chew.pw>
2018-03-30 11:19:57 -0500

    Added an %^assignrole and %^removerole command, for more role management

--- lib/plugins/roles.rb
+++ lib/plugins/roles.rb
@@ -11,4 +11,30 @@ module Roles
       event.respond "I do not have permission to 'manage roles', can you please provide those, thanks, appreciate ya"
     end
   end
+
+  command(:assignrole, min_args: 2, required_permissions: [:manage_roles], permission_message: 'My dude, you do not have permission to manage roles!') do |event, mention, *rolename|
+    id = Bot.parse_mention(mention.to_s).id.to_i
+    user = event.server.member(id)
+    rolename = rolename.join(' ')
+    begin
+      therole = event.server.roles.find { |role| role.name == rolename }
+      user.add_role(therole)
+      event.respond "I have successfully given #{user.distinct} the role `#{rolename}`!"
+    rescue Discordrb::Errors::NoPermission
+      event.respond "I do not have permission to 'manage roles', can you please provide those, thanks, appreciate ya"
+    end
+  end
+
+  command(:removerole, min_args: 2, required_permissions: [:manage_roles], permission_message: 'My dude, you do not have permission to manage roles!') do |event, mention, *rolename|
+    id = Bot.parse_mention(mention.to_s).id.to_i
+    user = event.server.member(id)
+    rolename = rolename.join(' ')
+    begin
+      therole = event.server.roles.find { |role| role.name == rolename }
+      user.remove_role(therole)
+      event.respond "I have successfully removed #{user.distinct} from the role `#{rolename}`!"
+    rescue Discordrb::Errors::NoPermission
+      event.respond "I do not have permission to 'manage roles', can you please provide those, thanks, appreciate ya"
+    end
+  end
 end
