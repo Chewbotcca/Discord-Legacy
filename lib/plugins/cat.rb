e57d34aa6e6852e6e4827b05c31d65c6d9016756
Chewsterchew <chew@chew.pw>
2018-03-30 09:07:24 -0500

    reinstate random.cat api

--- lib/plugins/cat.rb
+++ lib/plugins/cat.rb
@@ -2,13 +2,12 @@ module Cat
   extend Discordrb::Commands::CommandContainer
 
   command(:cat) do |event|
-    #whichcat = rand(2)
-    #showcat = if whichcat.zero?
-    #            JSON.parse(RestClient.get('http://aws.random.cat/meow'))['file']
-    #          else
-    #            Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
-    #          end
-    showcat = Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+    whichcat = rand(2)
+    showcat = if whichcat.zero?
+                JSON.parse(RestClient.get('http://aws.random.cat/meow'))['file']
+              else
+                Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+              end
     event.respond "#{['Aww!', 'Adorable.'].sample} #{showcat}"
   end
 
