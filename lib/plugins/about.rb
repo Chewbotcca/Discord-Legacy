e8932ef1d89c0eae593c48897f108e12fce7ed6e
Chewsterchew <chew@chew.pw>
2018-03-31 16:53:43 -0500

    Change wording, because sure.

--- lib/plugins/about.rb
+++ lib/plugins/about.rb
@@ -15,7 +15,7 @@ module About
   end
 
   command(:invite) do |event|
-    event.respond 'Hello! Invite me to your server here: <http://bit.ly/Chewbotcca>. Join the help server here: https://discord.gg/Q8TazNz'
+    event.respond 'Hello! Invite me to your server here: <http://bit.ly/Chewbotcca>. Join my help server here: https://discord.gg/Q8TazNz'
   end
 
   command(:forceupdateservercount) do |event|
