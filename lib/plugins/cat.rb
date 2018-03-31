f4f49890d4d4b5968a63aa378ae6258c906d85d2
Chewsterchew <chew@chew.pw>
2018-03-31 16:28:29 -0500

    Rescue broken cat command.

--- lib/plugins/cat.rb
+++ lib/plugins/cat.rb
@@ -3,11 +3,15 @@ module Cat
 
   command(:cat) do |event|
     whichcat = rand(2)
-    showcat = if whichcat.zero?
-                JSON.parse(RestClient.get('http://aws.random.cat/meow'))['file']
-              else
-                Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
-              end
+    begin
+      showcat = if whichcat.zero?
+                  JSON.parse(RestClient.get('http://aws.random.cat/meow'))['file']
+                else
+                  Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+                end
+    rescue RestClient::Forbidden
+      event.respond "There was an error grabbing the adorable kitten! It's probably the random cat api, just try again."
+    end
     event.respond "#{['Aww!', 'Adorable.'].sample} #{showcat}"
   end
 
