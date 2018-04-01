08443afb54ae119bd81d05a2e70bbae0eeb3ab47
Chewsterchew <chew@chew.pw>
2018-03-31 21:19:33 -0500

    Some eval improvements

--- lib/plugins/eval.rb
+++ lib/plugins/eval.rb
@@ -7,7 +7,17 @@ module Eval
       event.channel.send_embed do |e|
         e.title = '**Evaluated Successfully**'
 
-        e.description = eval code.join(' ')
+        evaluated = eval code.join(' ')
+
+        if evaluated.to_s.length > 1500
+          puts '---BEGIN EVALUATION OUTPUT---'
+          puts evaluated.to_s
+          puts '---END EVALUATION OUTPUT---'
+          evaluated = evaluated.to_s[0..1500]
+          e.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Please note the output was limited to 1500 characters due to discord having limits. The rest of the output is in your console.')
+        end
+
+        e.description = evaluated
         e.color = '00FF00'
       end
     rescue StandardError => f
