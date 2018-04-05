5ce95d57b0fe77285d50b06c7e8422439744d845
Chewsterchew <chew@chew.pw>
2018-04-05 18:33:15 -0500

    Embed cats if it can. Also just redo fails

--- lib/plugins/cat.rb
+++ lib/plugins/cat.rb
@@ -10,9 +10,18 @@ module Cat
                   Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
                 end
     rescue RestClient::Forbidden
-      event.respond "There was an error grabbing the adorable kitten! It's probably the random cat api, just try again."
+      redo
     end
-    event.respond "#{['Aww!', 'Adorable.'].sample} #{showcat}"
+    begin
+      event.channel.send_embed do |embed|
+        embed.title = ['Aww!', 'Adorable.'].sample
+        embed.url = showcat
+
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: showcat)
+      end
+    rescue Discordrb::Errors::NoPermission
+      event.respond "#{['Aww!', 'Adorable.'].sample} #{showcat}"
+    end
   end
 
   command(:catfact) do |event|
