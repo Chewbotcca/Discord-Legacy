aa0e50d55aef089ef063cd57528b9c85e464a9aa
Chewsterchew <chew@chew.pw>
2018-04-16 17:34:52 -0500

    Show how many messages have been pruned

--- lib/plugins/moderation.rb
+++ lib/plugins/moderation.rb
@@ -9,5 +9,18 @@ module Moderation
     rescue Discordrb::Errors::NoPermission
       event.respond "Come on dude, you can't just expect me to prune a channel without Manage Messages!!"
     end
+    begin
+      m = event.channel.send_embed do |embed|
+        embed.title = 'Messages Successfully Pruned'
+        embed.colour = 0xd084
+        embed.description = ":wastebasket: Say goodbye to #{howmany} messages!"
+
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'This message will automatically delete in 5 seconds.')
+      end
+      sleep 5
+      m.delete
+    rescue Discordrb::Errors::NoPermission
+      event.send_temporary_message(":wastebasket: Say goodbye to #{howmany} messages!", 5)
+    end
   end
 end
