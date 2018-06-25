5ad2e50667aace3fc220e8b4fb2ad3f1a7c07281
Chewsterchew <chew@chew.pw>
2018-06-25 13:13:52 -0500

    fix that little bitty error

--- lib/plugins/musicplayer.rb
+++ lib/plugins/musicplayer.rb
@@ -7,7 +7,7 @@ module MusicPlayer
   end
 
   command(:play) do |event, song|
-    song.downcase! unless song.nil?
+    song&.downcase!
     case song
     when 'mrcena'
       event.voice.play_file('data/music.mp3')
@@ -15,8 +15,6 @@ module MusicPlayer
       event.voice.play_file('data/rickroll.mp3')
     when 'wearenum1'
       event.voice.play_file('data/wearenum1.mp3')
-    else
-      event.respond 'Song doesnt exist in the database! Check database with `%^songs`'
     end
   end
 
