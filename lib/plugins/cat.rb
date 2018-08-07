b0dc98ae2837f766191cbebf3f2e7ad5d34993ff
Chewsterchew <chew@chew.pw>
2018-08-06 21:33:08 -0500

    woof

--- lib/plugins/cat.rb
+++ lib/plugins/cat.rb
@@ -24,6 +24,24 @@ module Cat
     end
   end
 
+  command(:cat) do |event|
+    begin
+      showdog = JSON.parse(RestClient.get('https://random.dog/woof.json'))['url']
+    rescue RestClient::Forbidden
+      redo
+    end
+    begin
+      event.channel.send_embed do |embed|
+        embed.title = ['Aww!', 'Adorable.'].sample
+        embed.url = showdog
+
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: showdog)
+      end
+    rescue Discordrb::Errors::NoPermission
+      event.respond "#{['Aww!', 'Adorable.'].sample} #{showdog}"
+    end
+  end
+
   command(:catfact) do |event|
     event.respond JSON.parse(RestClient.get('https://catfact.ninja/fact'))['fact']
   end
