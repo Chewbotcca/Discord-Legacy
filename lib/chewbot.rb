8bda6b3d235ab109004488c9ee99a365700de400
Chewsterchew <chew@chew.pw>
2018-11-13 17:29:31 -0600

    Take the load off of chewbot.rb

--- lib/chewbot.rb
+++ lib/chewbot.rb
@@ -1,23 +1,3 @@
-require 'dblruby'
-require 'discordrb'
-require 'rest-client'
-require 'json'
-require 'yaml'
-require 'nokogiri'
-require 'open-uri'
-require 'listcordrb'
-puts 'All dependencies loaded'
-
-CONFIG = YAML.load_file('config.yaml')
-puts 'Config loaded from file'
-
-DBL = DBLRuby.new(CONFIG['dbotsorg'], CONFIG['client_id'])
-puts 'Properly Instantiated DBL!'
-
-Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'],
-                                          client_id: CONFIG['client_id'],
-                                          prefix: ["<@#{CONFIG['client_id']}> ", CONFIG['prefix']]
-
 puts 'Initial Startup complete, loading all plugins...'
 
 Starttime = Time.now
--- run.rb
+++ run.rb
@@ -1,3 +1,21 @@
+require 'dblruby'
+require 'discordrb'
+require 'rest-client'
+require 'json'
+require 'yaml'
+require 'nokogiri'
+require 'open-uri'
+puts 'All dependencies loaded'
+
 puts "Let's start that bot!"
+CONFIG = YAML.load_file('config.yaml')
+puts 'Config loaded from file'
+
+DBL = DBLRuby.new(CONFIG['dbotsorg'], CONFIG['client_id'])
+puts 'Properly Instantiated DBL!'
+
+Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'],
+                                          client_id: CONFIG['client_id'],
+                                          prefix: ["<@#{CONFIG['client_id']}> ", CONFIG['prefix']]
 
 require_relative 'lib/chewbot'
